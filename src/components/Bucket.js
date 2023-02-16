646ffc4f1f7cc9b0feaa92c884864b60666a7ae5
Kenny Hung (he) <khung@instructors.2u.com>
2023-02-16 20:06:46 +0000

    Make project run

--- src/components/Bucket.js
+++ src/components/Bucket.js
@@ -27,17 +27,18 @@ function Bucket(props) {
     // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
     // TODO: Add a key attribute set to the value of the index position
     // Hint: use a ternary operator
-    <div className={ } key={}>
+    // TODO: Fix these properly
+    <div className={'bucket row'} key={index}>
 
       // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument
-      <div key={} onClick={}>
+      <div key={index} onClick={() => {} }>
           {/* TODO: Add the item text here */}
       </div>
       <div className="icons">
         // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties
-        <p onClick={}> ✏️</p>
+        <p onClick={() => {}}> ✏️</p>
         {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
-        <p onClick={}> 🗑️</p>
+        <p onClick={() => {}}> 🗑️</p>
       </div>
     </div>
   ));
