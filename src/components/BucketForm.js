15403b38d6ce15bc348f5e0fd45acb09e842b9e8
Kenny Hung (he) <khung@instructors.2u.com>
2023-02-16 20:21:41 +0000

    Set more eagerness level

--- src/components/BucketForm.js
+++ src/components/BucketForm.js
@@ -4,7 +4,6 @@ function BucketForm({ edit, onSubmit }) {
   const [input, setInput] = useState('');
   let [eagerness, setEagerness] = useState('');
 
-  // TODO: Use this array in the return statement below
   const eagernessLevel = ['high', 'medium', 'low']
 
   const handleSubmit = (e) => {
@@ -46,7 +45,6 @@ function BucketForm({ edit, onSubmit }) {
             {eagerness || 'Priority'}
           </button>
           <div className="dropdown-content">
-            {/* TODO: Add an onClick event that will set the corresponding eagerness level from the `eagernessLevel` array */}
             <p onClick={() => setEagerness(eagernessLevel[0])}>Must do</p>
             <p onClick={() => setEagerness(eagernessLevel[1])}>Want to do</p>
             <p onClick={() => setEagerness(eagernessLevel[2])}>Take it or leave it</p>
@@ -72,10 +70,9 @@ function BucketForm({ edit, onSubmit }) {
             {eagerness || 'Priority'}
           </button>
           <div className="dropdown-content">
-            {/* TODO: Add an onClick event that will set the corresponding eagerness level from the `eagernessLevel` array */}
-            <p onClick={() => { }}>Must do</p>
-            <p onClick={() => { }}>Want to do</p>
-            <p onClick={() => { }}>Take it or leave it</p>
+            <p onClick={() => setEagerness(eagernessLevel[0])}>Must do</p>
+            <p onClick={() => setEagerness(eagernessLevel[1])}>Want to do</p>
+            <p onClick={() => setEagerness(eagernessLevel[2])}>Take it or leave it</p>
           </div>
         </div>
         <button className="bucket-button">Update</button>
